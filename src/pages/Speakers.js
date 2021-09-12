40a6281537046a2e28adfbf6fc9f332fb7a5a79f
jatin verma <jv.jatinverma.jv@gmail.com>
2021-09-12 22:24:04 +0530

    speakers section created

--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./Speakers.css";
 
 const Speakers = () => {
-  return <div>SPEAKERS</div>;
+  return <div class = "speakers_sec">
+    <h1 class="heading">Speakers</h1>
+    <div class="container">
+        <div class="speakers">
+            <div class="speaker_sec1">
+                <div class="image_in"><img src="png_thinkface_papercut.png" alt="Error!"/><a href="">Speaker's name</a></div>
+                <div class="image_in"><img src="" alt="Error!"/><a href="">Speaker's name</a></div>
+                <div class="image_in"><img src="" alt="Error!"/><a href="">Speaker's name</a></div>
+            </div>
+            <div class="speaker_sec2">
+                <div class="image_in"><img src="" alt="Error!"/><a href="">Speaker's name</a></div>
+                <div class="image_in"><img src="" alt="Error!"/><a href="">Speaker's name</a></div>
+                <div class="image_in"><img src="" alt="Error!"/><a href="">Speaker's name</a></div>
+            </div>
+        </div>
+        <div class="image_out img7"></div>
+    </div>
+  </div>;
 };
 
 export default Speakers;
