da4be4df78d5f2c12687b6ee47181f4c5bdfeeff
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-09-14 17:58:13 +0530

    card

--- src/components/ComponentCard.jsx
+++ src/components/ComponentCard.jsx
@@ -1,57 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
-import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core';
-import { motion } from 'framer-motion';
+import { makeStyles, createTheme, ThemeProvider } from "@material-ui/core";
+import { motion } from "framer-motion";
 
-const useStyles = makeStyles((theme)=>{
+const useStyles = makeStyles((theme) => {
   return {
-
     inputContainer: {
       backgroundColor: "transparent",
       width: "200px",
-      padding: theme.spacing(3)
+      padding: theme.spacing(3),
     },
     centerContainer: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
     },
-    image:{
-      width: '200px'
-    }
-      
+    image: {
+      width: "200px",
+    },
   };
 });
 
 const theme = createTheme({
-  palette: {
-      
-  }
+  palette: {},
 });
 
-function ComponentCard({src}) {
+function ComponentCard({ src }) {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-      // animation
-      <motion.div
-      whileHover={{scale: 1.1}}
-      whileTap={{scale: 0.9}}
-      >
-
-        <Paper elevation={3} className={classes.inputContainer}>
-            <div className={classes.centerContainer}>
-            <img src={src} className={classes.image}/>
-            </div>
-        </Paper>
-
-      </motion.div>
-
-    );
-
+  return (
+    // animation
+    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+      <Paper elevation={3} className={classes.inputContainer}>
+        <div className={classes.centerContainer}>
+          <img src={src} className={classes.image} />
+        </div>
+      </Paper>
+    </motion.div>
+  );
 }
 
 export default ComponentCard;
