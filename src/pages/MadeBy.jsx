af97a3a1820988e577e1d92e2a47caf6c708667a
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-09-14 18:08:38 +0530

    card

--- src/pages/MadeBy.jsx
+++ src/pages/MadeBy.jsx
@@ -1,63 +1,126 @@
 import React from "react";
 import "./MadeBy.css";
 import MadeByIMG from "./MadeBy.jpg";
-import {FaFacebook,FaTwitter,FaLinkedin,FaInstagram} from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 function MadeBy(props) {
-  return <section id="MadeBy">Contributed by
-  <div className="flex">
-    <div className="card">
-      <div className="imgBx">
-        <img src={MadeByIMG} />
-      </div>
-      <div className="content">
-        <div className="details">
-          <h2>Alina Smith<br /><span>Senior Designer</span></h2>
-          <ul className="sci">
-            <li><a href="#"><FaFacebook /></a></li>
-            <li><a href="#"><FaTwitter /></a></li>
-            <li><a href="#"><FaLinkedin /></a></li>
-            <li><a href="#"><FaInstagram /></a></li>
-          </ul>
+  return (
+    <section id="MadeBy">
+      Contributed by
+      <div className="flex">
+        <div className="card">
+          <div className="imgBx">
+            <img src={MadeByIMG} />
+          </div>
+          <div className="content">
+            <div className="details">
+              <h2>
+                Alina Smith
+                <br />
+                <span>Senior Designer</span>
+              </h2>
+              <ul className="sci">
+                <li>
+                  <a href="#">
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaInstagram />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  
-    <div className="card">
-      <div className="imgBx">
-        <img src={MadeByIMG} />
-      </div>
-      <div className="content">
-        <div className="details">
-          <h2>Alina Smith<br /><span>Senior Designer</span></h2>
-          <ul className="sci">
-            <li><a href="#"><FaFacebook /></a></li>
-            <li><a href="#"><FaTwitter /></a></li>
-            <li><a href="#"><FaLinkedin /></a></li>
-            <li><a href="#"><FaInstagram /></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
 
-    <div className="card">
-      <div className="imgBx">
-        <img src={MadeByIMG} />
-      </div>
-      <div className="content">
-        <div className="details">
-          <h2>Alina Smith<br /><span>Senior Designer</span></h2>
-          <ul className="sci">
-            <li><a href="#"><FaFacebook /></a></li>
-            <li><a href="#"><FaTwitter /></a></li>
-            <li><a href="#"><FaLinkedin /></a></li>
-            <li><a href="#"><FaInstagram /></a></li>
-          </ul>
+        <div className="card">
+          <div className="imgBx">
+            <img src={MadeByIMG} />
+          </div>
+          <div className="content">
+            <div className="details">
+              <h2>
+                Alina Smith
+                <br />
+                <span>Senior Designer</span>
+              </h2>
+              <ul className="sci">
+                <li>
+                  <a href="#">
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaInstagram />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBx">
+            <img src={MadeByIMG} />
+          </div>
+          <div className="content">
+            <div className="details">
+              <h2>
+                Alina Smith
+                <br />
+                <span>Senior Designer</span>
+              </h2>
+              <ul className="sci">
+                <li>
+                  <a href="#">
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FaInstagram />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  </section>;
+    </section>
+  );
 }
 
 export default MadeBy;
