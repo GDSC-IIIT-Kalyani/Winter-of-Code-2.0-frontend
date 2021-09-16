c4eac70ae1ab3cd426dbf6c1005466d87c1ff5d6
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-09-16 21:10:15 +0530

    added js for navbar

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,39 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const [isMobView, setIsMobView] = useState(false);
+  const [isCrossBurger, setIsCrossBurger] = useState(false);
 
-//   const hamburger = document.querySelector(".navBar_hamburger");
-//   const navbar = document.querySelector(".navBar_links");
-//   const nav_links = document.querySelectorAll(".navBar_link-item");
+  const handleHamburgerClick = (event) => {
+    setIsMobView(!isMobView);
+    setIsCrossBurger(!isCrossBurger);
+    const nav_links = document.querySelectorAll(".navBar_link-item");
+    nav_links.forEach((link, number) => {
+      if (link.style.animation) link.style.animation = "";
+      else
+        link.style.animation = `fade 0.1s ease forwards ${number / 7 + 0.2}s`;
+    });
+  };
 
-//   hamburger.addEventListener("click", () => {
-//     navbar.classList.toggle("mob-view");
-//     nav_links.forEach((link, number) => {
-//       if (link.style.animation) link.style.animation = "";
-//       else link.style.animation = `fade 0.5s ease forwards ${number / 7 + 0.2}s`;
-//     });
-//     hamburger.classList.toggle('cross-burger');
-// });
-
-  return( 
+  return (
     <nav className="navBar">
-        <div class="navBar_container">
-            <ul class = "navBar_links">
-                <li class = "navBar_link-item"><a href="####">HOME</a></li>
-                <li class = "navBar_link-item"><a href="####">ABOUT</a></li>
-                <li class = "navBar_link-item"><a href="####">TIMELINE</a></li>
-                <li class = "navBar_link-item"><a href="####">SPONSORS</a></li>
-                <li class = "navBar_link-item"><a href="####">SPEAKERS</a></li>
-                <li class = "navBar_link-item"><a href="####">PROJECTS</a></li>
-                <li class = "navBar_link-item"><a href="####">CONTRIBUTED BY</a></li>
-            </ul>
-            <div class="navBar_hamburger">
-                <div class="navBar_line1"></div>
-                <div class="navBar_line2"></div>
-                <div class="navBar_line3"></div>
-            </div>
+      <div className="navBar_container">
+        <ul className={`navBar_links ${isMobView ? "mob-view" : null}`}>
+          <li className="navBar_link-item">
+            <a href="####">HOME</a>
+          </li>
+          <li className="navBar_link-item">
+            <a href="####">ABOUT</a>
+          </li>
+          <li className="navBar_link-item">
+            <a href="####">TIMELINE</a>
+          </li>
+          <li className="navBar_link-item">
+            <a href="####">SPONSORS</a>
+          </li>
+          <li className="navBar_link-item">
+            <a href="####">SPEAKERS</a>
+          </li>
+          <li className="navBar_link-item">
+            <a href="####">PROJECTS</a>
+          </li>
+          <li className="navBar_link-item">
+            <a href="####">CONTRIBUTED BY</a>
+          </li>
+        </ul>
+        <div
+          className={`navBar_hamburger ${
+            isCrossBurger ? "cross-burger" : null
+          }`}
+          onClick={handleHamburgerClick}
+        >
+          <div className="navBar_line1"></div>
+          <div className="navBar_line2"></div>
+          <div className="navBar_line3"></div>
         </div>
+      </div>
     </nav>
   );
 };
