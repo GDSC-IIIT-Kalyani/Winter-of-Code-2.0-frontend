4e32be94b9c8dd65fa787221ed767b2359bc8280
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-09-20 10:42:33 +0530

    resposive

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./components/home/Home";
 function App() {
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
+      <NavBar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/projects" exact component={Projects} />
