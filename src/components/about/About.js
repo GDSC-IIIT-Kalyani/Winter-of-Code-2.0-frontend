04b68d657dbf989c689e21062a0af3ef2d6b84e4
DragonNexa <tirtha070@gmail.com>
2021-09-23 22:50:19 +0530

    Added About section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,24 +4,17 @@ import "./About.css";
 function About() {
   return (
     <section id="About">
-      <div>
+      
+      <div class="Fish">
+        <video autoPlay loop muted>
+          <source src="./Assets/Fish_vid.mp4" type="video/mp4" />
+        </video>
+      </div>
+      
+      <div class="Papercut">
         <img src="./Assets/About_papercut.png" alt="" />
       </div>
-      {/* <div class="writings">
-        <h1>ABOUT</h1>
-      </div>
 
-      <div class="writings2">
-        <h1>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        </h1>
-      </div> */}
     </section>
   );
 }
