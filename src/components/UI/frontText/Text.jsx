323f39b6ebb3a5d8fe9173fdba7be6a5d8c46253
Divya <karan.boss1000@gmail.com>
2021-09-29 01:34:31 +0530

    Screen Resolution and some responsive changes

--- src/components/UI/frontText/Text.jsx
+++ src/components/UI/frontText/Text.jsx
@@ -65,7 +65,7 @@ function Text(props) {
         </Typography>
         <br />
         <Typography variant="h4" className={classes.text} align="center">
-          RESGISTER AND SUBMIT PROPOSAL
+          REGISTER AND SUBMIT PROPOSAL
         </Typography>
         <br />
         <Button
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import {FaTwitter,FaInstagram,FaLinkedin,FaGithub,FaCopyright} from "react-icons/fa";
+import {FaTwitter,FaInstagram,FaLinkedin,FaEnvelope,FaGithub,FaCopyright} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -9,6 +9,7 @@ const Footer = () => {
         <a href="https://twitter.com/gdsciiitkalyani" target="_blank" rel="noreferrer" className="icon twitter"><FaTwitter size = '5x'/></a>
         <a href="https://www.instagram.com/gdsciiitkalyani/" target="_blank" rel="noreferrer" className="icon instagram"><FaInstagram size = '5x'/></a>
         <a href="https://www.linkedin.com/company/gdsc-iiit-kalyani/" target="_blank" rel="noreferrer" className="icon linkedin"><FaLinkedin size = '5x'/></a>
+        <a href="https://mail.google.com/" target="_blank" rel="noreferrer" className="icon email"><FaEnvelope size = '5x'/></a>
         <a href="https://github.com/GDSC-IIIT-Kalyani" target="_blank" rel="noreferrer" className="icon github"><FaGithub size = '5x'/></a>
     </div>
     <div className="copyright">
