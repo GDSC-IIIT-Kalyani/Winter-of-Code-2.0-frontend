ccd553e87b2fa49fe9f8d80cf4f8a5617780a617
Akshit17 <akshit.chodhary@gmail.com>
2021-09-30 21:05:06 +0530

    Imported Yash

--- src/components/developers/teamData.js
+++ src/components/developers/teamData.js
@@ -1,6 +1,7 @@
 import MadeByIMG from "../../assets/images/profile.png";
 import Akshit from "../../assets/images/Developers/Akshit.jpg";
 import Prayag from "../../assets/images/Developers/Prayag.jpg";
+import Yash from "../../assets/images/Developers/Yash.jpeg";
 import Abhishree from "../../assets/images/Developers/Abhishree.jpg";
 import Divya from "../../assets/images/Developers/Divya.jpg";
 import Suryansh from "../../assets/images/Developers/Suryansh.jpg";
@@ -49,6 +50,17 @@ const developers = [
       linkedin: "https://www.linkedin.com/in/prayag-biswas-293644215/",
       twitter: null,
       instagram: "https://www.instagram.com/___dragon_x___/",
+    },
+  },
+  {
+    name: "Yash",
+    image: Yash,
+    role: "Designer & Developer",
+    socialMedia: {
+      github: "https://github.com/yashraj-02-techie",
+      linkedin: "https://www.linkedin.com/in/yashraj-singh-02/",
+      twitter: "https://twitter.com/yashraj_02",
+      instagram: "https://www.instagram.com/conficker_exe/",
     },
   },
   {
--- src/components/timeline/TimeLine.js
+++ src/components/timeline/TimeLine.js
@@ -5,7 +5,7 @@ import Vertical from "./Vertical";
 function TimeLine() {
   return (
     <section className="v-timeline" id="timeline">
-      <div className="v-head">Timelines</div>
+      <div className="v-head">Timeline</div>
       <div className="v-time">
         <Vertical />
       </div>
