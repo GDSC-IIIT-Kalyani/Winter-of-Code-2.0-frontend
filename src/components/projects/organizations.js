ac7421ad2075e83e04f8c7967854175df8252eca
yashraj-02-techie <yashrajsingh977@gmail.com>
2021-10-05 19:29:02 +0530

    Adds hoppscotch org

--- src/components/projects/organizations.js
+++ src/components/projects/organizations.js
@@ -1,6 +1,6 @@
 import React from "react";
 import skit from "../../assets/organizations/skit2.png";
-import llvm from "../../assets/organizations/llvm.png";
+import hoppscotch from "../../assets/organizations/Hoppscotch.png"
 import llvm2 from "../../assets/organizations/llvm_large.png";
 import aysncAPI from "../../assets/organizations/asyncAPI4.png";
 import edualgo2 from "../../assets/organizations/Edualgoalt.png";
@@ -13,22 +13,22 @@ function Orgs() {
     <div className="mid">
       <ul className="project_sponsors">
         <li>
-          <img src={skit} alt="sponsor_Logo" />
+          <img src={skit} alt="org_logo" />
         </li>
         <li>
-          <img src={aysncAPI} alt="sponsor_Logo" />
+          <img src={aysncAPI} alt="org_logo" />
         </li>
         <li>
-          <img src={jinaAi} alt="sponsor_Logo" />
+          <img src={jinaAi} alt="org_logo" />
         </li>
         <li>
-          <img src={llvm} alt="sponsor_Logo" />
+          <img src={llvm2} alt="org_logo" />
         </li>
         <li>
-          <img src={edualgo2} alt="sponsor_Logo" />
+          <img src={edualgo2} alt="org_logo" />
         </li>
         <li>
-          <img src={llvm2} alt="sponsor_Logo" />
+          <img src={hoppscotch} alt="org_logo" />
         </li>
       </ul>
       <div class="desc">ORGANISATIONS</div>
