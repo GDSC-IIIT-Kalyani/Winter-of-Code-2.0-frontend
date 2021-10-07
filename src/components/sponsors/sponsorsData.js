a907bd5a51a92eea794ec7cd5a9941d28866a15b
mohit355 <mohitraj772002@gmail.com>
2021-10-08 00:37:20 +0530

    added jet as gold

--- src/components/sponsors/sponsorsData.js
+++ src/components/sponsors/sponsorsData.js
@@ -3,28 +3,38 @@ import codeForCause from "../../assets/sponsors/codeForCause.png";
 import jetBrains from "../../assets/sponsors/jetBrains.jpg";
 import newtonSchool from "../../assets/sponsors/newtonSchool.png";
 import prepbytes from "../../assets/sponsors/prepbytes.png";
-import jinaAi from "../../assets/sponsors/jinaAi.png"
+import jinaAi from "../../assets/sponsors/jinaAi.png";
 import tezos from "../../assets/sponsors/tezos.png";
-import polygon from "../../assets/sponsors/polygon.jpg"
-import replit from "../../assets/sponsors/Replit.jpg"
-import noticebard from "../../assets/sponsors/noticebard.png"
-import filecoin from "../../assets/sponsors/filecoin.png"
-import celo from "../../assets/sponsors/celo.png"
-import hackclub from "../../assets/sponsors/hackclub.png"
-import devfolio from "../../assets/sponsors/devfolio.png"
-
+import polygon from "../../assets/sponsors/polygon.jpg";
+import replit from "../../assets/sponsors/Replit.jpg";
+import noticebard from "../../assets/sponsors/noticebard.png";
+import filecoin from "../../assets/sponsors/filecoin.png";
+import celo from "../../assets/sponsors/celo.png";
+import hackclub from "../../assets/sponsors/hackclub.png";
+import devfolio from "../../assets/sponsors/devfolio.png";
 
 const sponsorsData = [
   [
-    "Ivory tier",
+    "Ivory Sponsors",
     [
       { title: "Newton School", image: newtonSchool },
       { title: "1Stop.ai", image: oneStop },
       { title: "PrepBytes", image: prepbytes },
     ],
   ],
+  ["Diamond Sponsor", [{ title: "Devfolio", image: devfolio }]],
+  ["Diamond Sponsor", [{ title: "Repl.it", image: replit }]],
+
   [
-    "Platinum",
+    "Gold Sponsors",
+    [
+      { title: "Code For Cause", image: codeForCause },
+      { title: "Jet Brains", image: jetBrains },
+    ],
+  ],
+  ["Silver Sponsors", [{ title: "Hackclub", image: hackclub }]],
+  [
+    "Platinum Sponsors",
     [
       { title: "Tezos", image: tezos },
       { title: "Celo", image: celo },
@@ -32,23 +42,14 @@ const sponsorsData = [
     ],
   ],
   [
-    "Platinum",
+    "Platinum Sponsors",
     [
       { title: "FileCoin", image: filecoin },
       { title: "Jina AI", image: jinaAi },
     ],
   ],
-  ["Gold", [{ title: "Code For Cause", image: codeForCause }]],
-  [
-    "Diamond",
-    [
-      { title: "Devfolio", image: devfolio },
-      { title: "Repl.it", image: replit },
-    ],
-  ],
-  ["Sponsors", [{ title: "Jet Brains", image: jetBrains }]],
+
   ["Media Partner", [{ title: "Noticebard", image: noticebard }]],
-  ["Silver", [{ title: "Hackclub", image: hackclub }]],
   ["Sponsors", [{ title: "Coming Soon" }]],
 ];
 
