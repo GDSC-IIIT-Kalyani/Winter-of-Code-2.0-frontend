db0050721e5ec67471bffb41c556dd5b0d727989
unknown <suryu7240@gmail.com>
2021-10-11 15:28:19 +0530

    Devfolio button added

--- src/components/UI/frontText/Text.js
+++ src/components/UI/frontText/Text.js
@@ -49,12 +49,10 @@ const useStyles = makeStyles((theme) => {
       fontFamily: "'Asap', sans-serif",
       marginRight: "1em",
       marginLeft: "1em",
-      fontSize: "1.8vw",
+      fontSize: "1.6vw",
       borderRadius: "15px",
       background: "#ACA0F9",
       backgroundColor: "transparent",
-
-      // color: "black",
       color: "#FFFF",
     },
   };
@@ -80,7 +78,6 @@ const theme = createTheme({
 
 function Text(props) {
   const classes = useStyles();
-
   return (
     <ThemeProvider theme={theme}>
       <Container>
@@ -112,6 +109,41 @@ function Text(props) {
               style={{ textDecoration: "none", color: "white" }}
             >
               Register Now
+            </a>
+          </Button>
+          <Button
+            className={classes.btn}
+            variant="contained"
+            size="large"
+            color="primary"
+            onClick={() => console.log("clicked")}
+          >
+            <a
+              href="https://gdsc-woc.devfolio.co/"
+              target="_blank"
+              rel="noreferrer"
+              className="dev_logo"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <svg
+                className="logo"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 115.46 123.46"
+                fill="#fff"
+              >
+                <path
+                  d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 
+            12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 
+            13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 
+            2.18.08 3.34z"
+                ></path>
+                <path
+                  d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 
+            104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 
+            55.41 0 0 1 50.58 55.34z"
+                ></path>
+              </svg>
+              &ensp;Apply with Devfolio
             </a>
           </Button>
           {/* <Button
