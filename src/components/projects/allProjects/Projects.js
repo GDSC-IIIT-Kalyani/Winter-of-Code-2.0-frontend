078693e3fa44e1a730a43f7324bec58279336067
subhadeepchowdhury41 <subhadeepchowdhury41@gmail.com>
2021-11-11 18:38:14 +0530

    final design with filters and a new scrollbar

--- src/components/projects/allProjects/Projects.js
+++ src/components/projects/allProjects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Projects.css";
 import "../../header/NavBar";
 import SlideData from "./SlideData";
@@ -13,6 +13,26 @@ import {
 import "react-icons/fi";
 
 const Projects = () => {
+  let [proj_type, setproj_type] = useState("all");
+  let [proj_org, setproj_org] = useState("all");
+  const handleChange = (e) => {
+    setproj_type(e.target.value);
+  };
+  const handleOrg = (e) => {
+    setproj_org(e.target.value);
+  };
+  useEffect(() => {
+    const proj_form = document.querySelectorAll(".main_item");
+    proj_form.forEach((item) => {
+      item.addEventListener("click", () => {
+        item.children[1].classList.toggle("proj-active");
+        item.children[1].children[2].classList.toggle("ideas-active");
+        item.children[0].classList.toggle("proj-active-left");
+        item.children[0].children[0].children[0].children[0].classList.toggle('idea-hide')
+        item.children[0].classList.toggle('back-img');
+      });
+    });
+  }, []);
   const SlidingDiv = useRef(null);
   const calculatePoints = () => {
     var x = window.screen.width;
@@ -40,6 +60,34 @@ const Projects = () => {
           <source src={proj_video} type="video/mp4" />
         </video>
       </div>
+      <div className="filter_option">
+        <h3>Filter out by Categories</h3>
+        <form id="proj_form">
+          <select className="proj_slt" onChange={handleChange}>
+            <option value="all">All Categories</option>
+            <option value="web">Web Development</option>
+            <option value="app">App Development</option>
+            <option value="ml">Machine Learning</option>
+            <option value="cloud">Cloud</option>
+            <option value="doc">Documentation</option>
+            <option value="compiler">Compiler</option>
+            <option value="py">Python</option>
+          </select>
+          <select className="proj_slt" onChange={handleOrg}>
+            <option value="all">All organizations</option>
+            <option value="mojo">Mojo Global</option>
+            <option value="llvm">LLVM</option>
+            <option value="geeta">The Gita Initiative</option>
+            <option value="autodl">Auto DL</option>
+            <option value="hoppscotch">Hoppscotch</option>
+            <option value="jina">Jina AI</option>
+            <option value="edualgo">Edualgo Academy</option>
+            <option value="skit">Skit AI</option>
+            <option value="asyn">AsyncAPI</option>
+            <option value="gdsc">GDSC</option>
+          </select>
+        </form>
+      </div>
       <div className="Container-arrows">
         <div className="Arrow1" onClick={slideLeft}>
           <FaArrowLeft />
@@ -50,7 +98,11 @@ const Projects = () => {
       </div>
       <div ref={SlidingDiv} className="proj_slides">
         {SlideData.map((slide, idx) => {
-          return <ProjectCard key={idx} data={slide}/>;
+          if (slide.tags.includes(proj_type) && slide.org_tag.includes(proj_org)) {
+            return <ProjectCard key={idx} data={slide}/>;
+          } else {
+            return null;
+          }
         })}
       </div>
     </div>
